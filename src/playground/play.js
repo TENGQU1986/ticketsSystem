e429c65e4e93df9985220ddac86cf60b35d65bd1
Patrick Qu <quteng.bo@gmail.com>
2018-04-17 12:37:03 -0400

    js1

---
+++ src/playground/play.js
@@ -0,0 +1,17 @@
+// const obj = {
+//   name: 'Patrick',
+//   age: 25
+// };
+
+// const obj1 = {
+//   ...obj,
+//   job: 'developer'
+// };
+
+// console.log(obj1);
+
+const firstName = 'Patrick';
+const lastName = 'Qu';
+
+const val = firstName.concat(lastName);
+console.log(val);
