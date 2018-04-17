0491f4b3e83f16cb29084cee046687aae675dc2f
Patrick Qu <quteng.bo@gmail.com>
2018-04-17 17:00:42 -0400

    js2

--- src/playground/play.js
+++ src/playground/play.js
@@ -10,8 +10,16 @@
 
 // console.log(obj1);
 
-const firstName = 'Patrick';
-const lastName = 'Qu';
+// const firstName = 'Patrick';
+// const lastName = 'Qu';
 
-const val = firstName.concat(lastName);
-console.log(val);
+// const val = firstName.concat(lastName);
+// console.log(val);
+
+// const numbers = [23, 34, 4, 5, 56, 6, 7, 6, 8];
+// const numbers2 = new Array(34, 45, 56, 93);
+// console.log(numbers2);
+
+// console.log(window.navigator.platform);
+
+console.log(window.document.all);
